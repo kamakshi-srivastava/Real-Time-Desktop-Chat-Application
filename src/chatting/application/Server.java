b34a54f832eba6ddeccfda941aeefb17eb1828e2
kamakshi-srivastava <kamashriv@gmail.com>
2025-01-29 21:18:35 +0530

    Update Server.java

--- src/chatting/application/Server.java
+++ src/chatting/application/Server.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+//author- kamakshi-srivastava
 package chatting.application;
 import java.awt.*;
 import javax.swing.*;
@@ -11,10 +12,7 @@ import java.util.*;
 import javax.swing.border.*;
 import java.net.*;
 import java.io.*;
-/**
- *
- * @author HP
- */
+
 public class Server implements ActionListener{
     
     JTextField text;
